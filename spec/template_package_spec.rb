f29b6df2958cee49a74bf7d361c9398be2ca8dcc
Carlos Goce <carlosgoce@gmail.com>
2016-10-14 18:45:12 +0200

    Refactor spec

--- spec/template_package_spec.rb
+++ spec/template_package_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe Rodolfo::TemplatePackage do
   subject(:package) { Rodolfo::TemplatePackage.new simple_template_path }
 
   context 'when template package is correct' do
-    it 'should return its json schema' do
+    it 'returns a json schema' do
       expect(package.schema).to include_json(description: 'Simple Template')
     end
 
-    it 'should return a its proc maker function' do
+    it 'returns a proc maker function' do
       expect(package.proc_maker(msg: 'Hello')).to be_an_instance_of Proc
     end
   end
